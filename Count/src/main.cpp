cf39f4b183fe1c4c9534589221e2e58f68139610
Carsten Fehlaber <c.fehlhaber.genion.de>
2024-04-11 18:18:49 +0200

    Count neu

---
+++ Count/src/main.cpp
@@ -0,0 +1,22 @@
+/*
+ * main.cpp
+ *
+ *  Created on: 11.04.2024
+ *  Author: Carsten Fehlhaber
+ */
+
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+int main()
+{
+	int counter = 0;
+	string a;
+	cout << "Hochzähler" << endl;
+
+}
